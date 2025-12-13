81f31300cf59d99d8a0ce6657070e9d58c8f6c9d
Rahul G <rahulgirish110206@gmail.com>
2025-12-13 19:18:32 +0530

    Added NavigationBar
    -Fixed BrowserRouter
    -Added NavigationBar with links to Home and About
    -Added custom font 'Ubuntu'

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function Navbar() {
+function NavigationBar() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Navbar
+    <div className="relative bg-[#555555] text-[#edebeb]">
+
+      <button onClick={() => {
+          navigate("/");
+        }
+      }
+      className="text-3xl py-3 px-5"
+      >Activity Points Tracker</button>
+
+      <button onClick={() => {
+          navigate("/about");
+        }
+      }
+      className="absolute right-5 bottom-3 px-5 text-xl"
+      >About</button>
     </div>
   )
 }
 
-export default Navbar;
+export default NavigationBar;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavigationBar from "../components/Navbar.jsx"
+import Home from "../pages/Home.jsx";
+import About from "../pages/About.jsx";
+import Login from "../pages/Login.jsx";
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 
