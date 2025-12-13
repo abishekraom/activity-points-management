ffbf5db1d6a6ea3fcef1efc72ff1043442c0f5a7
Rahul G <rahulgirish110206@gmail.com>
2025-12-13 17:48:24 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/auth.js';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use("/api/users", router);
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`); 
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+
+function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+
+export default App;
